97e86e897c9fe52689ef737421a6ad866b97085f
macksih <mhbobeh.jamali@gmail.com>
2023-06-07 17:42:20 +0330

    added main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const body = document.querySelector('body');
 
 console.log(buttons);
 
+
 buttons.forEach(function(button){
     button.addEventListener('click', function(e){
         console.log(e.target);
